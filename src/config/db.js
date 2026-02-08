17a51592fab6e43d48d197fd0108e30471075da4
Salah-EddineArsalane <arsalane.salaheddine@gmail.com>
2026-02-08 21:07:39 +0100

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  if (!process.env.MONGO_URI) {
+    console.error("MONGO_URI is missing");
+    process.exit(1);
+  }
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
-    console.error(error);
+    console.error("DB connection error:", error);
     process.exit(1);
   }
 };
